af36e9609da525867ba4182e29ac292b6fd8c817
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-12-11 10:22:29 +0100

    Event component created

---
+++ front/src/components/Event/Event.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Event extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+          
+        }
+      }
+ 
+
+
+
+  render (){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Event;
