d1b76a1ffa47bf52eabe183e8a338d9518a0f9dd
Robert Marine <marine.robert51100@gmail.com>
2019-12-13 11:19:47 +0100

    Update button front

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonRedirectory from './family-button/ButtonRedirectory.component';
+
+function Home  (){
+    return (
+        <div>
+            <p> Hello </p>
+            <ButtonRedirectory/>
+        </div>
+
+    )
+
+}
+export default Home; 
+
