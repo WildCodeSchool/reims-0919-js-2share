076ecbc663f12259124086b77a2ef4b15a99fa4b
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-12-17 10:46:24 +0100

    add logout button

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ButtonRedirectory from './family-button/ButtonRedirectory.component';
-import LoginForm from './Loginform/LoginForm';
+import LogoutControl from './LogoutControl/LogoutControl'
 
 function Home  (){
     return (
         <div>
             <p> Hello </p>
+            <LogoutControl />
             <ButtonRedirectory/>
             <LoginForm />
         </div>
