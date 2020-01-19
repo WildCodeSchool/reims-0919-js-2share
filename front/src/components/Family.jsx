7eda4f9dbd432b78a13a90210748dbd40e765261
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-01-19 12:11:00 +0100

    get family members from server + implemented member creation

--- front/src/components/Family.jsx
+++ front/src/components/Family.jsx
@@ -4,20 +4,44 @@ class Family extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      members: [
-        {email: 'jessy@mail.com'},
-        {email: 'marine@mail.com'},
-        {email: 'alexis@mail.com'},
-        {email: 'sophie@mail.com'},
-      ]
+      members: [],
+      newMemberEmail: ''
     }
+    this.addMember = this.addMember.bind(this)
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:8000/families/${this.props.match.params.id}/users`)
+      .then(response => response.json())
+      .then(data => this.setState({members: data}))
+  }
+
+  addMember() {
+    fetch(`http://localhost:8000/families/${this.props.match.params.id}/users`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.newMemberEmail,
+        family_id: this.props.match.params.id,
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState(prevState => ({
+          members: [...prevState.members, data],
+          newMemberEmail: '',
+        }))
+      })
   }
 
   render() {
     return (
       <ul>
         <li>
-          <input type="email" placeholder="jdoe@mail.com" /><button>+</button>
+          <input type="email" placeholder="jdoe@mail.com" onChange={e => this.setState({newMemberEmail: e.target.value})} value={this.state.newMemberEmail} />
+          <button onClick={this.addMember}>+</button>
         </li>
         {
           React.Children.toArray(
