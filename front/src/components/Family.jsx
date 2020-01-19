d958cac4273a815f070a703dda061edad6b7ade7
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-01-19 10:41:27 +0100

    added static render in Family

--- front/src/components/Family.jsx
+++ front/src/components/Family.jsx
@@ -4,13 +4,33 @@ class Family extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      members: []
+      members: [
+        {email: 'jessy@mail.com'},
+        {email: 'marine@mail.com'},
+        {email: 'alexis@mail.com'},
+        {email: 'sophie@mail.com'},
+      ]
     }
   }
 
   render() {
     return (
-      <p>hello from /families/{this.props.match.params.id}</p>
+      <ul>
+        <li>
+          <input type="email" placeholder="jdoe@mail.com" /><button>+</button>
+        </li>
+        {
+          React.Children.toArray(
+            this.state.members.map(
+              member => (
+                <li>
+                  {member.email}<button>-</button>
+                </li>
+              )
+            )
+          )
+        }
+      </ul>
     )
   }
 }
