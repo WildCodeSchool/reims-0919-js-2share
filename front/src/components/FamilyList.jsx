38d655ff20b0dc5fbf1ced01845cffef1c29548b
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-01-19 11:09:01 +0100

    refactored FamilyList into a function component

--- front/src/components/FamilyList.jsx
+++ front/src/components/FamilyList.jsx
@@ -3,34 +3,27 @@ import {
   Link,
 } from "react-router-dom";
 
-class FamilyList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      families: this.props.families,
-    }
-  }
-
-  render() {
-    return (
-      <ul>
-        <li>
-          <input type="text" placeholder="Doe" /><button>+</button>
-        </li>
-        {
-          React.Children.toArray(
-            this.state.families.map(
-              family => (
-                <li>
-                  <Link to={`/families/${family.id}`}>{family.name}</Link>
-                </li>
-              )
+const FamilyList = ({
+  families,
+}) => {
+  return (
+    <ul>
+      <li>
+        <input type="text" placeholder="Doe" /><button>+</button>
+      </li>
+      {
+        React.Children.toArray(
+          families.map(
+            family => (
+              <li>
+                <Link to={`/families/${family.id}`}>{family.name}</Link>
+              </li>
             )
           )
-        }
-      </ul>
-    )
-  }
+        )
+      }
+    </ul>
+  )
 }
 
 export default FamilyList
