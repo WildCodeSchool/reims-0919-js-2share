8a701598491c93234a1a3bc231ff7d6b5570a2d3
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-01-19 10:30:51 +0100

    added static render in FamilyList

--- front/src/components/FamilyList.jsx
+++ front/src/components/FamilyList.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import {
+  Link,
+} from "react-router-dom";
 
 class FamilyList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      families: []
+      families: [
+        {id: 1, name: 'Pinchon'},
+        {id: 2, name: 'Robert'},
+        {id: 3, name: 'Tonello'},
+        {id: 4, name: 'Trochain'},
+      ]
     }
   }
 
   render() {
     return (
-      <p>hello from /families</p>
+      <ul>
+        <li>
+          <input type="text" placeholder="Doe" /><button>+</button>
+        </li>
+        {
+          React.Children.toArray(
+            this.state.families.map(
+              family => (
+                <li>
+                  <Link to={`/families/${family.id}`}>{family.name}</Link>
+                </li>
+              )
+            )
+          )
+        }
+      </ul>
     )
   }
 }
