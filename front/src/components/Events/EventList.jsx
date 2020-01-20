f05195684887c9050ccf2e206ba95910e0160439
Jessy Pinchon <jessy.pinchon@gmail.com>
2020-01-20 14:55:12 +0100

    add style on button

--- front/src/components/Events/EventList.jsx
+++ front/src/components/Events/EventList.jsx
@@ -6,8 +6,8 @@ function EventList (props){
   return(
     props.events.map(event => (
       <article className='event_list_style' key={event.id}>
-        <p>Début :{event.date_start.substring(10,16)}<br/>Fin :{event.date_end.substring(10,16)}</p>
         <h4 className='title_newEvent'>{event.title}</h4>
+        <p>Début :{event.date_start.substring(10,16)}<br/>Fin :{event.date_end.substring(10,16)}</p>
         <button className='test2' onClick={()=> props.removeEvent(event.id)}>
           <i className="fas fa-calendar-minus" aria-hidden="true">Supp</i>
         </button>
