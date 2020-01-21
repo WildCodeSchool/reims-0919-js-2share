53762c6317432c34f5c7b9eff4003b3de7dfdbfd
Aloxbro <alox51@wanadoo.fr>
2020-01-21 17:35:52 +0100

    fixed front to get/post on the right area

--- front/src/components/FamilyList.jsx
+++ front/src/components/FamilyList.jsx
@@ -1,35 +1,36 @@
-import React, {
-  useState,
-} from 'react'
-import {
-  Link,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const FamilyList = ({
-  families,
-  createFamily,
-}) => {
-  const [newFamilyName, setNewFamilyName] = useState('')
+const FamilyList = ({ families, createFamily, token }) => {
+  const [newFamilyName, setNewFamilyName] = useState("");
 
   return (
     <ul>
       <li>
-        <input type="text" placeholder="Doe" onChange={e => setNewFamilyName(e.target.value)} value={newFamilyName} />
-        <button onClick={() => {createFamily(newFamilyName); setNewFamilyName('')}}>+</button>
+        <input
+          type="text"
+          placeholder="Doe"
+          onChange={e => setNewFamilyName(e.target.value)}
+          value={newFamilyName}
+        />
+        <button
+          onClick={() => {
+            createFamily(newFamilyName);
+            setNewFamilyName("");
+          }}
+        >
+          +
+        </button>
       </li>
-      {
-        React.Children.toArray(
-          families.map(
-            family => (
-              <li>
-                <Link to={`/families/${family.id}`}>{family.name}</Link>
-              </li>
-            )
-          )
-        )
-      }
+      {React.Children.toArray(
+        families.map(family => (
+          <li>
+            <Link to={`/families/${family.id}`}>{family.name}</Link>
+          </li>
+        ))
+      )}
     </ul>
-  )
-}
+  );
+};
 
-export default FamilyList
+export default FamilyList;
