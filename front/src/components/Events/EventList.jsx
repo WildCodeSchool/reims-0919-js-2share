db997cc8a881b1fd2a868b34e99c3541246902d1
Sophie <strochain@gmail.com>
2020-01-24 15:58:59 +0100

    add style for demonstration

--- front/src/components/Events/EventList.jsx
+++ front/src/components/Events/EventList.jsx
@@ -8,7 +8,7 @@ function EventList (props){
       <article className="space:inset text:center" key={event.id}>
         <h4 className="space:inline">{event.title}</h4>
         <p className="flex:1 space:inset-squish space:inline">Début :{event.date_start.substring(10,16)}<br/>Fin :{event.date_end.substring(10,16)}</p>
-        <button className="flex-self:center space:inset-squish"  onClick={()=> props.removeEvent(event.id)}>-</button>
+        <button className="flex-self:center space:inset-squish"  onClick={()=> props.removeEvent(event.id)}>Supprimer</button>
       </article>
       )
     )
